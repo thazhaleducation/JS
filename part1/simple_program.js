8f4504c6cd38d14ec89d66d52684a56b491ec0af
Raj Bharath AgileLab <raj.bharath@agilelab.it>
2022-11-08 12:50:24 +0530

    Add part1

---
+++ part1/simple_program.js
@@ -0,0 +1,16 @@
+name = prompt("Enter your name");
+age = prompt("Enter your age");
+married = prompt("Enter your marital status (Yes/No)");
+pincode = prompt("Enter your pincode");
+
+console.log("Name: ");
+console.log(name);
+
+console.log("Age: ");
+console.log(age);
+
+console.log("Married: ");
+console.log(married);
+
+console.log("Pincode: ");
+console.log(pincode);
